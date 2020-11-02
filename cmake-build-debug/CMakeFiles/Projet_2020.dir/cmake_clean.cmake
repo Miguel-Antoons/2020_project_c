1c96b47ebe7f007479dfe83c945915fbea1c048e
Miguel-Antoons <miguel.antoons@gmail.com>
2020-11-02 16:25:22 +0100

    "First commit"

---
+++ cmake-build-debug/CMakeFiles/Projet_2020.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projet_2020.dir/main.c.o"
+  "Projet_2020"
+  "Projet_2020.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projet_2020.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
