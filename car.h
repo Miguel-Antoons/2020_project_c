fb78eeaa34cb7ec2c7cdc8e53153d0d07e9e4e3b
Maxime Danlee <max@interplanet.be>
2020-11-05 05:19:56 -0800

    test

--- car.h
+++ car.h
@@ -6,3 +6,17 @@
 #define PROJET_2020_CAR_H
 
 #endif //PROJET_2020_CAR_H
+
+struct Car {
+    int idCar;
+    double s1;
+    double s2;
+    double s3;
+    double totalLap;
+    // best time
+    double bestS1;
+    double bestS2;
+    double bestS3;
+    double BestLap;
+
+};
